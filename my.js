c2532c5418944cef1431699f9e6404a0c85358bc
hlh112 <84497856+hlh112@users.noreply.github.com>
2021-05-20 16:45:35 +0800

    Add files via upload

---
+++ my.js
@@ -0,0 +1,93 @@
+$(window).scroll(function() {
+    skipButtonToggle();
+});
+
+$(document).ready(function(){
+	init();
+});
+
+function init() {
+    onloadFadeIn();
+	initCarousel();
+    initChangeImg();
+    scrollToggle();
+}
+
+function onloadFadeIn() {
+    $('body').fadeIn(700);
+    $('.skip-button').css("display", "none")
+}
+
+function initCarousel() {
+
+   $('.carousel-wrapper').click(function(){
+        var $this = $(this),
+            $Img = $this.find('.active'),
+            $currentImg = $Img.is(":last-child");
+
+        $Img.removeClass('active');
+
+        if ($currentImg  == true) {
+            $Img.siblings().first().addClass('active');
+        } else {
+            $Img.next().addClass('active');
+        }
+   })
+}
+
+function initChangeImg() {
+    $('.finger-tap').click(function(){
+        var $this = $(this),
+            $fingerAttr = $this.attr('data-finger-count'),
+            $img = $('.switch-img');
+
+            $this.addClass('active');
+            $this.siblings().removeClass('active');
+
+            $img.each(function(){
+                var $this = $(this),
+                    $imgAttr = $this.attr('data-switch-count');
+
+                if ($fingerAttr == $imgAttr) {
+                    $this.addClass('active');
+                    $this.siblings().removeClass('active');
+                } else {}
+            })
+    })
+}
+
+function scrollToggle() {
+    var elm = document.getElementById("back-button");
+    if (elm != null){
+
+    var prevScrollpos = window.pageYOffset;
+
+    window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("back-button").style.opacity = "1";
+    } else {
+        document.getElementById("back-button").style.opacity = "0";
+    }
+
+    prevScrollpos = currentScrollPos;
+
+    }
+    } else {}
+}
+
+function skipButtonToggle() {
+    var $elm = $('#skip-button'),
+        $currentPos = window.pageYOffset;
+        $uiSection = $('#UIOverview').offset();
+    
+    if ($elm.length) {
+
+        if ($currentPos < $uiSection.top) {
+            $elm.fadeIn();
+        } else {
+            $elm.fadeOut();
+        }
+    } else {}
+}
